01c393a39652f54edf5dbe42e593365649e1d568
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-05-26 18:22:47 -0600

    fix env variable

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.spoonacular.com/recipes',
 	headers: {
-		'x-api-key': import.meta.env.VITE_APP_API_KEY as string,
+		'x-api-key': process.env._API_KEY as string,
 	},
 });
