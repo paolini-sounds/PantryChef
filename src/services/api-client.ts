fdfe2b858a48dd5b269f6e8c62ae0c37ddec9a95
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-05-26 18:29:36 -0600

    fixed apikey

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.spoonacular.com/recipes',
 	headers: {
-		'x-api-key': process.env._API_KEY as string,
+		'x-api-key': import.meta.env.VITE_API_KEY as string,
 	},
 });
