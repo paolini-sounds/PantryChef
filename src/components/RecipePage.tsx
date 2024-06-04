20a9b3018b678cc764f6142a112c0f4df5ca3f0c
paolini-sounds <120350296+paolini-sounds@users.noreply.github.com>
2024-06-04 13:31:03 -0600

    fixed styles

--- src/components/RecipePage.tsx
+++ src/components/RecipePage.tsx
@@ -3,6 +3,7 @@ import {
 	Button,
 	Card,
 	CardBody,
+	Divider,
 	Flex,
 	GridItem,
 	Heading,
@@ -38,65 +39,80 @@ const RecipePage = () => {
 	const steps = recipe.analyzedInstructions[0].steps;
 
 	return (
-		<Box padding={10}>
+		<Box paddingY={{ base: 5, lg: 10 }} paddingX={{ base: 2, md: 10, lg: 100 }}>
 			<Button onClick={() => navigate('/')}>Back</Button>
-			<VStack margin={{ sm: 2, md: 5 }}>
+			<VStack margin={{ sm: 0, md: 5 }}>
 				<Heading paddingY={5}>{recipe.title}</Heading>
 			</VStack>
-			<Card
-				borderRadius={10}
-				overflow='hidden'
-				direction={{ sm: 'column', lg: 'row' }}
+			<Flex
+				direction='column'
 				width='100%'
-				margin={{ sm: 2, md: 5 }}
-				justifyContent='space-evenly'
+				alignItems='center'
+				justifyContent='center'
 			>
-				<Image maxHeight='100%' objectFit='cover' src={recipe.image} />
-				<CardBody
-					width='50%'
-					rowGap={5}
-					justifyContent='flex-start'
-					marginX={{ sm: 0, lg: 5 }}
+				<Card
+					maxWidth={{ base: '1000px', md: '500px', lg: '1200px' }}
+					minWidth={{ base: 'md', lg: '800px' }}
+					borderRadius={10}
+					overflow='hidden'
+					direction={{ base: 'column', lg: 'row' }}
+
+					// justifyContent={{ sm: 'flex-start', md: 'space-evenly' }}
 				>
-					<RecipeIconList size='lg' recipe={recipe} />
+					<Flex width={{ base: '100%', lg: '50%' }}>
+						<Image objectFit='contain' src={recipe.image} />
+					</Flex>
 
-					<Heading size='sm' marginY={3}>
-						Ready in: {displayReadyTime(recipe.readyInMinutes)}
-					</Heading>
+					<CardBody
+						marginX={{ base: 0, lg: 5 }}
+						width={{ base: '100%', lg: '50%' }}
+					>
+						<RecipeIconList size='lg' recipe={recipe} />
 
-					<Heading marginY={3} size='sm'>
-						Servings: {recipe.servings}
-					</Heading>
+						<Heading size='sm' marginY={3}>
+							Ready in: {displayReadyTime(recipe.readyInMinutes)}
+						</Heading>
 
-					<Heading marginY={2} size='md'>
-						Ingredients:
-					</Heading>
+						<Heading marginY={3} size='sm'>
+							Servings: {recipe.servings}
+						</Heading>
+						<Divider />
+						<Heading marginY={2} size='md'>
+							Ingredients:
+						</Heading>
+						<List>
+							<SimpleGrid columns={{ sm: 1, md: 2 }}>
+								{recipe.extendedIngredients.map((ingredient: any) => (
+									<GridItem key={ingredient.id}>
+										<ListItem fontSize='sm'>
+											<ListIcon as={RiKnifeLine} color='green' />
+											{ingredient.amount} {ingredient.unit} {ingredient.name}
+										</ListItem>
+									</GridItem>
+								))}
+							</SimpleGrid>
+						</List>
+					</CardBody>
+				</Card>
+
+				<Box
+					paddingLeft={10}
+					maxWidth={{ base: '1000px', md: '500px', lg: '1000px' }}
+					minWidth={{ base: 'md', lg: '800px' }}
+				>
 					<List>
-						<SimpleGrid columns={{ sm: 1 }}>
-							{recipe.extendedIngredients.map((ingredient: any) => (
-								<GridItem key={ingredient.id}>
-									<ListItem fontSize='sm'>
-										<ListIcon as={RiKnifeLine} color='green' />
-										{ingredient.amount} {ingredient.unit} {ingredient.name}
-									</ListItem>
-								</GridItem>
-							))}
-						</SimpleGrid>
+						<Heading marginY={{ base: 5, lg: 10 }} size='lg'>
+							Instructions
+						</Heading>
+						{steps.map((step: any) => (
+							<ListItem key={step.number}>
+								<ListIcon as={PiCookingPotBold} color='green' />
+								{step.number}: {step.step}
+							</ListItem>
+						))}
 					</List>
-				</CardBody>
-			</Card>
-
-			<List>
-				<Heading marginY={2} size='lg'>
-					Instructions
-				</Heading>
-				{steps.map((step: any) => (
-					<ListItem key={step.number}>
-						<ListIcon as={PiCookingPotBold} color='green' />
-						{step.number}: {step.step}
-					</ListItem>
-				))}
-			</List>
+				</Box>
+			</Flex>
 		</Box>
 	);
 };
